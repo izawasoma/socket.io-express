6350b58039148474191f2b5b67de49bc53588ded
izawasoma <izawasoma@gmail.com>
2022-03-15 17:01:52 +0900

    チャット機能を作成

---
+++ routes/socketIO.js
@@ -0,0 +1,14 @@
+const socketio = require('socket.io');
+function socketIO(server) {
+    const sio = socketio(server);
+    sio.on('connection', function(socket) {
+        socket.on('chat message', (msg) => {
+            console.log('message: ' + msg);
+            socket.emit('chat message', msg);
+        });
+        socket.on("disconnect", function() {
+            console.log('user disconnected');
+        });
+    });
+};
+module.exports = socketIO;
