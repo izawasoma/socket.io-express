6441666eaa401976f025189afcd4d9ce8a998a12
izawasoma <izawasoma@gmail.com>
2022-03-20 04:46:15 +0900

    ディレクトリ構成を変更

--- socketIO/socketIO.js
+++ socketIO/socketIO.js
@@ -2,7 +2,6 @@ const socketio = require('socket.io');
 function socketIO(server) {
     const sio = socketio(server);
     sio.on('connection', function(socket) {
-        console.log("接続しました");
         socket.on('chat message', (msg) => {
             console.log('message: ' + msg);
             sio.emit('chat message', msg);
