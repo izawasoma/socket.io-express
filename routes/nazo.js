a67d970a32133341ec17c653b6e7464fe794f943
izawasoma <izawasoma@gmail.com>
2022-03-20 20:19:33 +0900

    引っ越ししました

--- routes/nazo.js
+++ routes/nazo.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Socket.io + Express' });
+router.get('/nazo', function(req, res, next) {
+  res.render('./nazo.ejs');
 });
 
 module.exports = router;
